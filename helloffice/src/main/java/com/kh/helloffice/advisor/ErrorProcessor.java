4ea51712f81d456b865444bcb798cc5266adaba1
itssu <ssujinn96@gmail.com>
2022-03-17 16:19:48 +0900

    ✨ 404에러 핸들링

--- helloffice/src/main/java/com/kh/helloffice/advisor/ErrorProcessor.java
+++ helloffice/src/main/java/com/kh/helloffice/advisor/ErrorProcessor.java
@@ -1,18 +1,22 @@
 package com.kh.helloffice.advisor;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import lombok.extern.slf4j.Slf4j;
 
-@ControllerAdvice(annotations = Controller.class)
+@ControllerAdvice
 @Slf4j
 public class ErrorProcessor {
+	
+	@ExceptionHandler(NoHandlerFoundException.class)
+	@ResponseStatus(value = HttpStatus.NOT_FOUND)
+	public String handle404(Exception e) {
+		log.info(e.toString());
+		return "error/exception404";
+	}
 
-//	@ExceptionHandler(Exception.class)
-//	public String errorProcess(Exception e) {
-//		log.info(e.toString());
-//		return "error/exception";
-//	}
 }
